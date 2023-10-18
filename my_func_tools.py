9049d699162e283a46d5b6530843c050872b97db
kawr13 <62603910+kawr13@users.noreply.github.com>
2023-10-18 08:58:33 +0500

    realise file save json

--- my_func_tools.py
+++ my_func_tools.py
@@ -1,52 +1,56 @@
-# Асинхронный веб-скраппер для извлечения данных с веб-страниц
-
 import asyncio
 import aiohttp
 from bs4 import BeautifulSoup as bs
+import aiofiles
+from concurrent.futures import ThreadPoolExecutor
 
-# Функция для получения HTML-кода страницы
-async def srappers(url: str):
-    async with aiohttp.ClientSession() as session:
+
+async def scrap(url: str):
+    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(limit=100)) as session:
         async with session.get(url) as response:
             if response.status == 200:
                 return await response.text()
 
-# Функция для извлечения ссылок на статьи с веб-страницы
-async def soup_text(sites: str):
+async def extract_urls(sites: str):
     soup = bs(sites, 'lxml')
-    url_detailed = soup.select('a.tm-title__link')
-    data = [f"https://habr.com{url.get('href')}" for url in url_detailed]
+    url_detailed = soup.find_all('a', class_='tm-title__link')
+    data = [f"https://habr.com{url['href']}" for url in url_detailed]
     return data
 
-# Функция для получения данных со страницы статьи
 async def fetch_data(url):
-    async with aiohttp.ClientSession() as session:
+    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(limit=100)) as session:
         async with session.get(url) as response:
             if response.status == 200:
                 return await response.text()
 
-# Функция для обработки каждой страницы статьи
 async def process_page(url):
     data = await fetch_data(url)
     soup = bs(data, 'lxml')
-    words = soup.select_one('div.tm-article-presenter')
+    words = soup.find('div', class_='tm-article-presenter')
     header = words.find('h1').text
-    text = [i.text for i in soup.select('p')]
+    text = [i.text for i in soup.find_all('p')]
     dat_parse = {'header': header, 'text': text}
     return dat_parse
 
-# Основная функция для выполнения всего процесса
+async def file_save(data: list):
+    async with aiofiles.open('data.json', 'w', encoding='utf-8') as f:
+        await f.write(str(data))
+
 async def main(url: str):
     sites_dat = []
-    sites_data = await srappers(url)
-    data = await soup_text(sites_data)
-    coros = [process_page(i) for i in data]
-    results = await asyncio.gather(*coros)
+    sites_data = await scrap(url)
+    data = await extract_urls(sites_data)
+
+    with ThreadPoolExecutor() as executor:
+        loop = asyncio.get_event_loop()
+        tasks = [process_page(url) for url in data]
+        results = await asyncio.gather(*tasks)
+
     sites_dat.extend(results)
-    return sites_dat
+    await file_save(sites_dat)
 
 if __name__ == "__main__":
     answ = 'python'
     URL = rf'https://habr.com/ru/search/?q={answ}&target_type=posts&order=relevance'
-    date = asyncio.run(main(URL))
-    print(date)
+    asyncio.run(main(URL))
+    
